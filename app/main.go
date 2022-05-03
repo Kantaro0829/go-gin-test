54acd936e80dc684e9fd4d465d9352ad1b9f2135
Kantaro0829 <eshangwan@gmail.com>
2022-05-04 04:41:19 +0900

    update main.go

--- app/main.go
+++ app/main.go
@@ -1,18 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+type User struct {
+	gorm.Model
+	Mail     string
+	Password []byte
+	Age      uint8
+}
+
+//
 func main() {
-	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:ecc@tcp(db:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+	db.AutoMigrate(&User{})
+	// Create
+	//db.Create(&Product{Code: "D42", Price: 100})
+
+	//ハッシュ
+	hash, err := bcrypt.GenerateFromPassword([]byte("plain text"), 12)
+	if err != nil {
+		panic("failed to hash password")
+	}
+	fmt.Println(hash)
+
 	engine := gin.Default()
 	engine.GET("/", func(c *gin.Context) {
+	    user := User{Mail: "test@gmail.com", Password: hash, Age: 12}
+	    result := db.Create(&user)
+        if result.Error != nil {
+            panic("failed to insert the data to the users table")
+            fmt.Println(result.Error)
+        }
+        fmt.Println(user.ID)
 		c.JSON(http.StatusOK, gin.H{
 			"message": "hello world",
 		})
--- db/mysql_init/init.sql
+++ db/mysql_init/init.sql
@@ -1,27 +1,27 @@
 CREATE DATABASE IF NOT EXISTS test;
 use test;
 
-CREATE TABLE user (
-    user_id INT(8) NOT NULL,
-    user_name VARCHAR(200) NOT NULL,
-    password varchar(270) NOT  NULL,
-    mail varchar(30) unique
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- CREATE TABLE user (
+--     user_id INT(8) NOT NULL,
+--     user_name VARCHAR(200) NOT NULL,
+--     password varchar(270) NOT  NULL,
+--     mail varchar(30) unique
+--     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE user
-  ADD PRIMARY KEY (`user_id`);
-
-
+-- ALTER TABLE user
+--   ADD PRIMARY KEY (`user_id`);
 
 
 
-ALTER TABLE user
-  MODIFY user_id int(11) AUTO_INCREMENT,AUTO_INCREMENT=1;
 
-INSERT INTO user (user_name, password, mail)
-        VALUES ("test一郎",
-                "paspaspas",
-                "2example@gmail.com");
+
+-- ALTER TABLE user
+--   MODIFY user_id int(11) AUTO_INCREMENT,AUTO_INCREMENT=1;
+
+-- INSERT INTO user (user_name, password, mail)
+--         VALUES ("test一郎",
+--                 "paspaspas",
+--                 "2example@gmail.com");
         
 
 
